fe0a1a818d8590065d0f3e48b8b22644f6206a79
Ashish Pathak <pathak.ashish87@gmail.com>
2016-06-08 23:03:19 +0530

    Correct the definition of order

--- app/src/main/java/com/aviras/mrassistant/models/Order.java
+++ app/src/main/java/com/aviras/mrassistant/models/Order.java
@@ -17,9 +17,57 @@ public class Order {
 
     private Doctor doctor;
 
-    private List<Medicine> medicines = new ArrayList<>();
+    private List<OrderItem> items = new ArrayList<>();
 
     private long createdDate;
     private long expectedDeliveryDate;
     private long actualDeliveryDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(long createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public long getExpectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public void setExpectedDeliveryDate(long expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+
+    public long getActualDeliveryDate() {
+        return actualDeliveryDate;
+    }
+
+    public void setActualDeliveryDate(long actualDeliveryDate) {
+        this.actualDeliveryDate = actualDeliveryDate;
+    }
 }
