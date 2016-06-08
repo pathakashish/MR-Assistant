c2c04766f5ebf4e05c5b7eb2c3ddba968557cc42
Ashish Pathak <pathak.ashish87@gmail.com>
2016-06-08 22:56:58 +0530

    Add model for order item

---
+++ app/src/main/java/com/aviras/mrassistant/models/OrderItem.java
@@ -0,0 +1,51 @@
+package com.aviras.mrassistant.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Represent order item
+ * <p/>
+ * Created by ashish on 8/6/16.
+ */
+public class OrderItem extends RealmObject {
+
+    private int medicineId;
+
+    private String medicineName;
+
+    private Unit unit;
+
+    private float quantity;
+
+    public int getMedicineId() {
+        return medicineId;
+    }
+
+    public void setMedicineId(int medicineId) {
+        this.medicineId = medicineId;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+}
